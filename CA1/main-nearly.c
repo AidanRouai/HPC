f3cb79732449297c1d39e614683404fdc8a97b54
AidanRouai <aidanrouai@gmail.com>
2025-04-28 16:58:01 +0100

    feat: add Makefile and main-nearly.c for stencil processing

---
+++ CA1/main-nearly.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "file-reader.c"
+#include "stencil.c"
+
+int main(int argc, char **argv) {
+    // Parse filenames from argv[1..3]
+    // Read input dims and data
+    // Read filter dims and data
+    // Allocate output array
+    // Call stencil function with timing
+    // Write output to file
+}
